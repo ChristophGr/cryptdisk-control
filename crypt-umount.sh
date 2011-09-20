c700924478a91b3cabb9cf2ea5f9afbbc8efdc2f
Christoph Gritschenberger <profalbert@bender.(none)>
2011-09-20 10:07:33 +0200

    fix syntax error

--- crypt-umount.sh
+++ crypt-umount.sh
@@ -2,7 +2,7 @@
 umount "/dev/mapper/$1" || exit 1
 cryptsetup luksClose $1
 CODE=$?
-if [ "$CODE" == "241" ]; then # busy
+if [ "$CODE" = "241" ]; then # busy
   dmsetup remove luks0
 elif [ "$CODE" != "0" ]; then
   exit 1;
