3b7ebcba0638c7db31282ef164d9d8b9995434ce
Christoph Gritschenberger <profalbert@bender.(none)>
2011-09-20 09:56:52 +0200

    make umount more resistent to errors

--- crypt-umount.sh
+++ crypt-umount.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-umount "/dev/mapper/$1" && cryptsetup luksClose $1 && rm -r "/media/$1"
+umount "/dev/mapper/$1" || exit 1
+cryptsetup luksClose $1
+CODE=$?
+if [ "$CODE" == "241" ]; then # busy
+  dmsetup remove luks0
+elif [ "$CODE" != "0" ]; then
+  exit 1;
+fi
+rmdir "/media/$1"
