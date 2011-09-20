b60cce720b0ceb43bcbb65d72531a41b85d3060c
Christoph Gritschenberger <profalbert@bender.(none)>
2011-09-20 09:45:52 +0200

    do not fail if dir exists

--- crypt-mount.sh
+++ crypt-mount.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo $3 | cryptsetup luksOpen "$1" "$2" && mkdir "/media/$2" && mount "/dev/mapper/$2" "/media/$2"
+echo $3 | cryptsetup luksOpen "$1" "$2" && mkdir -p "/media/$2" && mount "/dev/mapper/$2" "/media/$2"
